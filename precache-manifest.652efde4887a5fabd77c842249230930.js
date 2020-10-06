e9fdfa527fd40fa36aa4664eca25cb738f4a5c53
beck_pham <chauvupham@gmail.com>
2020-10-06 12:44:33 -0700

    Updates

---
+++ precache-manifest.652efde4887a5fabd77c842249230930.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b602f3f62d3ebbdbcc77e58a72245e6f",
+    "url": "/ISGMS-Tool/index.html"
+  },
+  {
+    "revision": "f9ee5addb69bed0c1617",
+    "url": "/ISGMS-Tool/static/css/main.5e5a75b7.chunk.css"
+  },
+  {
+    "revision": "da5aaa1aede3fa6fa758",
+    "url": "/ISGMS-Tool/static/js/2.370d1888.chunk.js"
+  },
+  {
+    "revision": "5f2ac350033ab5c4bebfa6b5635c1ace",
+    "url": "/ISGMS-Tool/static/js/2.370d1888.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9ee5addb69bed0c1617",
+    "url": "/ISGMS-Tool/static/js/main.aa4a2436.chunk.js"
+  },
+  {
+    "revision": "ba60c27b73270a6f4c04",
+    "url": "/ISGMS-Tool/static/js/runtime-main.f39e882f.js"
+  },
+  {
+    "revision": "1583c85170e28adff771a0e6415b616e",
+    "url": "/ISGMS-Tool/static/media/Andrew.1583c851.jpg"
+  },
+  {
+    "revision": "24250339d682b01822471484ae255aa5",
+    "url": "/ISGMS-Tool/static/media/Eng.24250339.jpg"
+  },
+  {
+    "revision": "e808ee69e645c0ed3acb7cfbd88716a4",
+    "url": "/ISGMS-Tool/static/media/Ly.e808ee69.jpg"
+  },
+  {
+    "revision": "cd436cc5468f9a8757e321531faa3544",
+    "url": "/ISGMS-Tool/static/media/Rick.cd436cc5.jpg"
+  },
+  {
+    "revision": "96ea2b29dd0d93052e926093cda10af5",
+    "url": "/ISGMS-Tool/static/media/Shaun.96ea2b29.jpg"
+  },
+  {
+    "revision": "32c6d0c094b3a468f7112a0f55fe3259",
+    "url": "/ISGMS-Tool/static/media/Siva.32c6d0c0.jpg"
+  },
+  {
+    "revision": "02bdd334894c1826bf57ef09417cd43c",
+    "url": "/ISGMS-Tool/static/media/Vanna.02bdd334.jpg"
+  },
+  {
+    "revision": "db35095194d7924e5b5ef43c5dd6f476",
+    "url": "/ISGMS-Tool/static/media/Vic.db350951.jpg"
+  },
+  {
+    "revision": "bd6820f1a4015e9645e470ea8e7b1501",
+    "url": "/ISGMS-Tool/static/media/anime3.bd6820f1.png"
+  },
+  {
+    "revision": "9c16b66f11811071e7ab95578904a5ea",
+    "url": "/ISGMS-Tool/static/media/automation.9c16b66f.svg"
+  },
+  {
+    "revision": "305c1f55fde48cc0b9222d506bcdd1f9",
+    "url": "/ISGMS-Tool/static/media/cloudvault.305c1f55.svg"
+  },
+  {
+    "revision": "6e915b3007ee973e69cbbb0ad556453b",
+    "url": "/ISGMS-Tool/static/media/datacenter.6e915b30.jpg"
+  },
+  {
+    "revision": "0037bcc9540fed5a29e571c1f672e56a",
+    "url": "/ISGMS-Tool/static/media/equipment.0037bcc9.svg"
+  },
+  {
+    "revision": "b10531a88934b89dc294e40803948cd4",
+    "url": "/ISGMS-Tool/static/media/feature-1.b10531a8.svg"
+  },
+  {
+    "revision": "70153fb1edf4593c2d130c9a3bbd0e5a",
+    "url": "/ISGMS-Tool/static/media/feature-2.70153fb1.svg"
+  },
+  {
+    "revision": "98433df0803bdb351a3768ed5d2c0082",
+    "url": "/ISGMS-Tool/static/media/feature-3.98433df0.svg"
+  },
+  {
+    "revision": "b0ed686dc57193e55b3980bd35a5548e",
+    "url": "/ISGMS-Tool/static/media/feature-4.b0ed686d.svg"
+  },
+  {
+    "revision": "3e9b2fa2188629c774d814386dbb073a",
+    "url": "/ISGMS-Tool/static/media/inventory.3e9b2fa2.svg"
+  },
+  {
+    "revision": "5fbc06d9f797b357cf73c9c8596cf491",
+    "url": "/ISGMS-Tool/static/media/iticket.5fbc06d9.svg"
+  },
+  {
+    "revision": "832bcfaf005adc3d20a68f86fc3933a7",
+    "url": "/ISGMS-Tool/static/media/kb.832bcfaf.svg"
+  },
+  {
+    "revision": "d2c087176f0d604043c761fb2e68dceb",
+    "url": "/ISGMS-Tool/static/media/logo-footer.d2c08717.png"
+  },
+  {
+    "revision": "74f355972764e12d7d79876eeb27480a",
+    "url": "/ISGMS-Tool/static/media/monitor.74f35597.svg"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/ISGMS-Tool/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/ISGMS-Tool/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/ISGMS-Tool/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/ISGMS-Tool/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "e04a1134737e4ae3927a462d5f5ae329",
+    "url": "/ISGMS-Tool/static/media/systeminfo.e04a1134.svg"
+  },
+  {
+    "revision": "5a8f5eadd1b4df8082739f7c8ecb79ac",
+    "url": "/ISGMS-Tool/static/media/timeseries.5a8f5ead.svg"
+  },
+  {
+    "revision": "f13e8fa08078b1a7228fab291592c4c9",
+    "url": "/ISGMS-Tool/static/media/troubleshooting.f13e8fa0.svg"
+  },
+  {
+    "revision": "54d339ba327afe3dc577486330415776",
+    "url": "/ISGMS-Tool/static/media/warranty.54d339ba.svg"
+  }
+]);
